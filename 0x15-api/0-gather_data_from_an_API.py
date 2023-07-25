abe8005f1149c56e191a9355bb874e01100c0803
vagrant <datsitsre>
2023-07-25 13:30:10 +0000

    Task 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ Rest API to retrieve some information """
-import requests 
+import requests
 import sys
 if __name__ == "__main__":
     rest_url = "https://jsonplaceholder.typicode.com/"
-    user_name = requests.get(rest_url + "user_names/{}".format(sys.argv[1])).json()
-    commd = requests.get(rest_url + "commd", params={"user_nameId": sys.argv[1]}).json()
+    user_name = requests.get(rest_url +
+                             "user_names/{}".format(sys.argv[1])).json()
+    commd = requests.get(rest_url +
+                         "commd", params={"user_nameId": sys.argv[1]}).json()
 
     completed = [t.get("title") for t in commd if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
