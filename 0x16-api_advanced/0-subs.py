e46f9308c446b3110d52b5880c529d0a6cb56691
vagrant <datsitsre>
2023-08-11 14:52:09 +0000

    0 subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,14 +5,16 @@ Query the Reddit Api
 import json
 import requests
 
+
 def number_of_subscribers(subreddit):
     '''
     Query for the subcribers
     '''
     url_link = "https://www.reddit.com/r/{}/about.json"
 
-    get_subcribers = requests.get(url_link.format(subreddit), headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
+    get_subcribers = requests.get(url_link.format(subreddit),
+                                  headers={"User-Agent": "My-User-Agent"},
+                                  allow_redirects=False)
 
     if get_subcribers.status_code >= 300:
         return 0
