df65ddaf49b24ba850afd24dc6f4d0c76165cae3
vagrant <datsitsre>
2023-08-11 14:23:11 +0000

    0 Subcribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+Query the Reddit Api
+'''
+import json
+import requests
+
+def number_of_subscribers(subreddit):
+    '''
+    Query for the subcribers
+    '''
+    url_link = "https://www.reddit.com/r/{}/about.json"
+
+    get_subcribers = requests.get(url_link.format(subreddit), headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+
+    if get_subcribers.status_code >= 300:
+        return 0
+
+    return get_subcribers.json().get("data").get("subscribers")
